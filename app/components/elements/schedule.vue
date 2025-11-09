3bf641b021e6ab40d8a87a98f04ccff8b0a80283
Adrian Popescu <adrianpopescu90@gmail.com>
2025-11-09 14:41:14 +0100

    General overall improvements
    Add schedule component.
    Update all translation strings.

---
+++ app/components/elements/schedule.vue
@@ -0,0 +1,26 @@
+<template>
+    <h4 class="mt-4">{{ $t("index.cta.hours.heading") }}</h4>
+    <ul class="mt-2 space-y-1">
+        <li
+            v-for="day in [
+                'monday',
+                'tuesday',
+                'wednesday',
+                'thursday',
+                'friday',
+                'saturday',
+                'sunday'
+            ]"
+            :key="day"
+            class="flex items-center justify-between border-b border-gray-200 pb-1">
+            <span>
+                {{ $t(`index.cta.hours.${day}.day`) }}
+            </span>
+            <UBadge
+                variant="soft"
+                :color="$t(`index.cta.hours.${day}.time`) === 'Closed' ? 'error' : 'success'">
+                {{ $t(`index.cta.hours.${day}.time`) }}
+            </UBadge>
+        </li>
+    </ul>
+</template>
